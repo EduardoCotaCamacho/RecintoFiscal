ecd4a653a50f46e06c9e2099e3af2c32f01057d9
EduardoC <EduardoC@EduardoC-PC>
2018-02-01 14:35:08 -0600

    proyecto inicial datos

---
+++ src/main/java/com/grucas/recintofiscal/model/Entrada.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.grucas.recintofiscal.model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author EduardoC
+ */
+public class Entrada {
+    
+  Integer id;
+  Integer orden;
+  Integer division;
+  String orden_cliente;
+  Integer cliente_grucas;
+  Integer linea_transporte;
+  Integer consignatario;
+  Integer cliente;
+  String descripcion_producto;
+  Integer peso;
+  Integer numero_bultos;
+  Integer numero_piezas;
+  Integer tipo_empaque;
+  String dgr_peligrosa;
+  String imo_peligrosa;
+  Integer dimension_largo;
+  Integer dimension_ancho;
+  Integer dimension_alto;
+  Integer volumen;
+  Integer puerto_origen;
+  Integer puerto_descarga;
+  Integer agente_aduanal;
+  String talon;
+  Double importe;
+  String viaje;
+  String barco;
+  Date eta;
+  String bill_lading;
+  String contenedor;
+  String marcas;
+  Date cierre_bodega;
+  String ubicacion;
+  String observaciones;
+  Integer estado;
+  String usuario;
+  Date alta;
+  Date modifica;
+}
